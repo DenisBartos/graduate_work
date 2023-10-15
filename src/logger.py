7c34bf312eb85ec4ae229bf42f5c65f8901b09c4
Денис Бартош <130225991+DenisBartos@users.noreply.github.com>
2023-10-15 23:15:39 +0300

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,8 @@
 import logging
-
 from data import LOGGER_FORMAT, LOGGER_DATE_FORMAT
 
 
 class Logger(logging.Logger):
-
     def __init__(self, name: str):
         super().__init__(name=name)
         stream_handler = logging.StreamHandler()
